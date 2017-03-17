3eb8520e54175638447a7270efc9a8c60e88d167
Ognjen Jevremovic <ognjen.jevremovic@sbgenomics.com>
2017-03-18 00:56:52 +0100

    Compiled tests

---
+++ tests/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var valid_1 = require("./valid");
+exports.validRGBValue_returnsString = valid_1.validRGBValue_returnsString;
+var invalid_1 = require("./invalid");
+exports.invalidRGBValue_returnError = invalid_1.invalidRGBValue_returnError;
+//# sourceMappingURL=index.js.map
---
+++ tests/invalid.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//  Dependancie
+var assert = require("assert");
+var rgbToHEX = require("..");
+//  Should return an Error
+var shouldReturnError = 'Should return an instance of Error ->';
+/**
+ * @description
+ *  Check if the value returns
+ *  a new instance of Error
+ *  and does not throw it
+ *
+ * @param {string} info
+ * @param {*} [value]
+ * @returns {*}
+ */
+function invalidRGBValue_returnError(info) {
+    var value = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        value[_i - 1] = arguments[_i];
+    }
+    it(shouldReturnError + " " + info, function () {
+        //  Does not throw an error
+        assert.doesNotThrow(function () { return rgbToHEX(value); }, Error);
+        //  But returns an instance of Error class
+        assert.deepEqual(function () { return rgbToHEX(value); }, new Error('error'));
+    });
+}
+exports.invalidRGBValue_returnError = invalidRGBValue_returnError;
+//# sourceMappingURL=invalid.js.map
